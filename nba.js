43793efbaa4fd73c54b99a656bd79431b5f52887
Matt-Tung88 <43249799+Matt-Tung88@users.noreply.github.com>
2020-05-06 18:41:01 -0400

    another update

--- nba.js
+++ nba.js
@@ -1,18 +1,57 @@
-var request = new XMLHttpRequest()
-request.open('GET', 'https://www.balldontlie.io/api/v1/players', TRUE);
-request.onload = function() {
-    var data = JSON.parse(this.response)
-    if(request.status >= 200 && request.status < 400) {
-        data.forEach( player => {
-            console.log(player.first_name)
+var playersList = [];
+// let search = document.getElementById("search");
+// search.addEventListener("keyup", (n) => {
+//     const searchString = n.target.value.toLowerCase();
+//     // console.log(n.target.value);
+//     const filteredPlayers = playersList.filter((player) => {
+//         return (
+//             player.toLowerCase().includes(searchString)
+//         );
+//     });
+//     return filteredPlayers;
+//     // displayPlayer(filteredPlayers);
+// });
+
+
+function loadPlayers() {
+
+    fetch('https://www.balldontlie.io/api/v1/players')
+        .then((res) => {
+            return res.json();
         })
-    } else {
-        alert('Error');
-    }
+        .then((data) => {
+            for(var i = 0; i < data.data.length; i++){
+                var name = data.data[i].first_name + " " + data.data[i].last_name;
+                // console.log(data.data[i].first_name);
+              
+
+                console.log(playersList.push(name));
+                console.log(typeof(name))
+                // console.log(typeof(playersList))
+            }
+            // console.log(playersList);
+            // console.log(data.data[1].first_name);
+            return data;
+
+        })
+
 }
 
-request.send();
 
-function getSearch() {
+loadPlayers();
 
-}
+console.log(playersList);
+
+
+// function createList(players) {
+//     var ul = document.createElement("ul");
+//     var li = document.createElement("li");
+//     for(var i = 0; i < players.length; i++) {
+
+//         li.appendChild(document.createTextNode(players[i]));
+//         ul.appendChild(li);
+//     }
+//     console.log(ul);
+// }
+
+// createList(playersList);
--- server.js
+++ server.js
@@ -1,28 +1,47 @@
-let express = require("express");
-let fetch = require('node-fetch');
+const express = require("express");
+const fetch = require("node-fetch");
 
-let app = express();
-let port = process.env.PORT || 3000;
+const app = express();
+const port = process.env.PORT || 3000;
 
+// app.use(express.static('src'));
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(express.static('nba.js'));
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// app.use(express.static('nba.js'));
 
-app.get('/api/v1/players', (req, res) => {
-    let url = 'https://www.balldontlie.io/api/v1/players';
-    fetch(url)
-        .then((r) => r.json())
-        .then((r) => r.map((dataset) => dataset))
-        .then((data) => {
-            console.log("Hi");
-            res.send({ data: data });
+app.get('/', (req, res) => {
+    // const url = 'https://www.balldontlie.io/api/v1/players';
+    // fetch(url)
+    //     .then((r) => r.json())
+    //     .then((r) => r.map((dataset) => dataset))
+    //     .then((data) => {
+    //         console.log(data);
+    //         res.send({ data: data });
+    //     })
+    //     .catch((err) => {
+    //         console.log(err);
+    //         res.redirect('/error');
+    
+    fetch('https://www.balldontlie.io/api/v1/players') 
+        .then((res) => {
+            return res.json();
         })
-        .catch((err) => {
-            console.log(err);
-            res.redirect('/error');
+        .then((data) => {
+            console.log(data);
+            res.send(data);
         });
 });
 
+// app.get('/', (req, res) => {
+//     fetch('https://www.balldontlie.io/api/v1/teams')
+//         .then((response) => {
+//             return response.json();
+//         })
+//         .then((data) => {
+//             console.log(data);
+//             res.send(data);
+//         });
+// });
 
 app.listen(port, () => console.log(`Your app is running on https://nba-project-mt.herokuapp.com/ and port: ${port}!`));
